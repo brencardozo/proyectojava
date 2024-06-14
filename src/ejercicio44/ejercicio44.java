b363c45fd38c8741a0a301c9cd7e8ceb21d79690
brencardozo <brendacarsozo20@gmail.com>
2024-06-14 12:05:30 -0300

    14-06

--- src/ejercicio44/ejercicio44.java
+++ src/ejercicio44/ejercicio44.java
@@ -1,14 +1,41 @@
 package ejercicio44;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Scanner;
+import java.util.HashSet;
 
 public class ejercicio44 {
 
 	public static void main(String[] args) {
-
+		Scanner datos = new Scanner(System.in);
+		
 		List<String> lista = new ArrayList<>();
-
+		
+	
+        System.out.println("Ingrese los equipos (escriba 'fin' para terminar):");
+        String entrada = datos.nextLine();
+	
+		
+	while(!entrada.equalsIgnoreCase("fin")) {
+		lista.add(entrada);
+		entrada  = datos.nextLine();
+		
+		
 	}
-}
+	HashSet<String> set = new HashSet<>(lista);
+	
+	System.out.println("\tLISTA CREADA");
+		for (String elemento : lista) {
+			
+			System.out.println(elemento);
+			
+			lista.clear();
+			lista.addAll(set);
+			
+		}
+		}
+		
+	}
+
